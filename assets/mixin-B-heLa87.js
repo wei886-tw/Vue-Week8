3c7f075222eee2be49c3cced21db59abd4bdeaee
Wei <lunhowl@gmail.com>
2024-05-31 16:44:18 +0800

    Updates

--- assets/mixin-B-heLa87.js
+++
@@ -1,1 +0,0 @@
-const n={methods:{loadingCircle(){let e=this.$loading.show({container:this.fullPage?null:this.$refs.formContainer,canCancel:!0,onCancel:this.onCancel});setTimeout(()=>{e.hide()},500)}}};export{n as m};
