57f574fb7fc997813baa1f884951eab645c411ac
Wei <lunhowl@gmail.com>
2024-06-02 19:51:05 +0800

    Updates

---
+++ assets/mixin-COaF9ylJ.js
@@ -0,0 +1,1 @@
+const i={methods:{loadingCircle(){let e=this.$loading.show({container:this.fullPage?null:this.$refs.formContainer,canCancel:!0,onCancel:this.onCancel});setTimeout(()=>{e.hide()},1e3)},checkAdmin(){const e=`${this.url}/api/user/check`;this.$http.post(e).then(()=>{this.getPageArticle(1)}).catch(t=>{alert(t.response.data.message),this.$router.push("/adminLogin")})}}};export{i as m};
