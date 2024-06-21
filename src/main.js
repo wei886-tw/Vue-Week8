ec5254d45c7c13a42e180a120027193cac22dc6c
Wei <lunhowl@gmail.com>
2024-06-21 16:08:52 +0800

    [test]

--- src/main.js
+++ src/main.js
@@ -27,10 +27,19 @@ import { localize, setLocale } from '@vee-validate/i18n';
 // 引入 VeeValidate 的繁體中文語系檔
 import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 
+defineRule('all', value => {
+  // 你的驗證邏輯
+  if (value !== '') {
+    return true;
+  }
+  return 'This field is required';
+});
 
 // 使用 Object.keys 將 AllRules 轉為陣列並使用 forEach 迴圈將驗證規則加入 VeeValidate
-Object.keys(AllRules).forEach(rule => {
-  defineRule(rule, AllRules[ rule ]);
+// 定義自定義驗證規則
+defineRule('all', value => {
+  // 你的驗證邏輯
+  return value !== '';
 });
 
 // 將當前 VeeValidate 的語系設定為繁體中文
